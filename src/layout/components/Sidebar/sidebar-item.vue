632c3c1a0060b0597a97af66d248e2799bfb2523
shisanss <15558105385@163.com>
2021-10-29 16:11:05 +0800

    Merge branch 'master' into DEMAND_DEV/wangc

--- src/layout/components/Sidebar/sidebar-item.vue
+++ src/layout/components/Sidebar/sidebar-item.vue
@@ -1,18 +1,10 @@
 <template>
   <div v-if="showMenu" class="menu-wrapper">
-<<<<<<< HEAD
     <template v-if="isOnlyOne">
       <app-link v-if="lastChild.meta" :to="resolvePath(basePath, lastChild.path)">
         <el-menu-item :index="resolvePath(basePath, lastChild.path)" :class="{ 'submenu-title-noDropdown': !props.isNest }">
           <svg-icon :icon-class="lastChild.meta.icon || (props.item.meta && props.item.meta.icon)" />
           <template #title class="system-menu-title">{{ lastChild.meta.title }}</template>
-=======
-    111
-    <template v-if="isOnlyOne">
-      <app-link v-if="lastChild.meta" :to="resolvePath(basePath, lastChild.path)">
-        <el-menu-item :index="resolvePath(basePath, lastChild.path)" :class="{ 'submenu-title-noDropdown': !props.isNest }">
-          <item :icon="lastChild.meta.icon || (props.item.meta && props.item.meta.icon)" :title="lastChild.meta.title" />
->>>>>>> master
         </el-menu-item>
       </app-link>
     </template>
