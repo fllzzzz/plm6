264d7610669ac888cd9181bdb75e126a355aca0a
shisanss <15558105385@163.com>
2021-12-29 17:36:04 +0800

    新增页面： 我的项目

--- src/views/mes/projects/index.vue
+++ src/views/mes/projects/index.vue
@@ -1,3 +1,164 @@
 <template>
-  <div>111</div>
+  <div class="app-wrap">
+    <div class="project-chart">
+      <projectChart v-model:year="year" />
+    </div>
+    <div class="app-container">
+      <!--工具栏-->
+      <div class="head-container">
+        <mHeader />
+      </div>
+      <!--表格渲染-->
+      <common-table
+        ref="tableRef"
+        v-loading="crud.loading"
+        :data="crud.data"
+        :empty-text="crud.emptyText"
+        :max-height="maxHeight"
+        style="width: 100%"
+      >
+        <el-table-column prop="index" label="序号" align="center" width="60" type="index" fixed="left"/>
+        <el-table-column v-if="columns.visible('serialNumber')" key="serialNumber"  prop="serialNumber" :show-overflow-tooltip="true" min-width="130" align="center"  label="合同编号" fixed="left" />
+        <el-table-column v-if="columns.visible('name')" key="name" prop="name" :show-overflow-tooltip="true" min-width="150" align="center"  label="项目名称"  />
+        <el-table-column v-if="columns.visible('businessType')" key="businessType" prop="businessType" label="业务类型"  align="center" min-width="100" >
+          <template v-slot="scope">
+            <span v-empty-text>{{ scope.row.businessType && businessTypeEnum.VL[  scope.row.businessType] }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column  v-if="columns.visible('planStartDate')" key="planStartDate" prop="planStartDate" label="计划开工日期"   align="center"  width="100"  >
+          <template v-slot="scope">
+            <span v-parse-time="'{y}-{m}-{d}'">{{ scope.row.planStartDate }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-if="columns.visible('planEndDate')" key="planEndDate" prop="planEndDate" label="计划完成日期" align="center" width="100" >
+          <template v-slot="scope">
+            <div v-parse-time="'{y}-{m}-{d}'">{{ scope.row.planEndDate }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column v-if="columns.visible('allDays')" key="allDays" prop="allDays" label="工期(天)" align="center" width="100">
+          <template v-slot="scope">
+            <div>{{ scope.row.allDays }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column  v-if="columns.visible('alreadyDays')" key="alreadyDays" prop="alreadyDays" label="已用时(天)" align="center" width="100" >
+          <template v-slot="scope">
+            <div>{{ scope.row.alreadyDays }}</div>
+          </template>
+        </el-table-column>
+      </common-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { getProjectList as get, getProjectInfo } from '@/api/common'
+import { ref, watch, provide, reactive } from 'vue'
+
+import useMaxHeight from '@compos/use-max-height'
+import useCRUD from '@compos/use-crud'
+import pagination from '@crud/Pagination'
+import mHeader from './module/header'
+import projectChart from './project-chart'
+import { businessTypeEnum } from '@enum-ms/contract'
+import { isNotBlank } from '@data-type/index'
+import { dateDifference } from '@/utils/date'
+
+// crud交由presenter持有
+const permission = {
+  get: ['projects:get']
+}
+
+const optShow = {
+  add: false,
+  edit: false,
+  del: false,
+  download: false
+}
+
+// 项目汇总数据（子页面使用）
+const projectInfo = reactive({
+  summary: {}, // 项目汇总数量
+  provinceList: [], // 省份项目数量汇总
+  loading: true
+})
+
+const tableRef = ref()
+const year = ref() // 年份（子组件使用）
+
+provide('projectInfo', projectInfo)
+
+const { crud, columns, CRUD } = useCRUD(
+  {
+    title: '我的项目',
+    permission: { ...permission },
+    optShow: { ...optShow },
+    crudApi: { get },
+    hasPagination: true
+  },
+  tableRef
+)
+
+watch(
+  () => year,
+  (val) => {
+    fetchProjectInfo()
+    if (crud.query.year !== val.value) {
+      crud.query.year = val.value
+      crud.toQuery()
+    }
+  },
+  { deep: true }
+)
+
+const { maxHeight } = useMaxHeight({ paginate: true })
+
+CRUD.HOOK.handleRefresh = (crud, data) => {
+  data.data.content = data.data.content.map((v) => {
+    // 计算天数
+    v.allDays = ''
+    v.alreadyDays = ''
+    if (isNotBlank(v.planStartDate)) {
+      // 工期
+      if (isNotBlank(v.planEndDate)) {
+        v.allDays = dateDifference(v.planStartDate, v.planEndDate)
+      }
+      // 用时天数（清单内所有任务全部入库，自动停止计时）
+      let endDate = v.planEndDate || v.inWarehouseDate
+      if (isNotBlank(v.planEndDate) && isNotBlank(v.inWarehouseDate)) {
+        endDate = Math.min(v.planEndDate, v.inWarehouseDate)
+      }
+      if (isNotBlank(endDate)) {
+        v.alreadyDays = dateDifference(v.planStartDate, endDate)
+      }
+    }
+    return v
+  })
+}
+
+// 获取项目汇总数据
+async function fetchProjectInfo() {
+  projectInfo.loading = true
+  try {
+    const res = await getProjectInfo({ year: year.value }) || {}
+    projectInfo.provinceList = res.provinceList
+    delete res.provinceList
+    projectInfo.summary = res
+  } catch (error) {
+    console.log('获取项目汇总图表数据', error)
+  } finally {
+    projectInfo.loading = false
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-wrap {
+    display: flex;
+    .app-container {
+      flex: 1;
+      min-width: 0;
+    }
+  }
+</style>
---
+++ src/views/mes/projects/project-chart/module/chart.vue
@@ -0,0 +1,171 @@
+<template>
+  <div :id="id" :style="{width}" />
+</template>
+
+<script setup>
+import { ref, computed, watch, defineProps, inject, onMounted, onBeforeUnmount } from 'vue'
+
+import * as echarts from 'echarts'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  width: {
+    type: String,
+    default: '100%'
+  },
+  height: {
+    type: String,
+    default: '400px'
+  }
+})
+
+const projectInfo = inject('projectInfo')
+
+const chart = ref()
+const provinceList = ref([])
+
+const provinceNameList = computed(() => {
+  return provinceList.value.map(v => v.name)
+})
+
+const quantityList = computed(() => {
+  return provinceList.value.map(v => v.quantity)
+})
+
+const fontWidth = computed(() => {
+  let num = Math.max(...provinceNameList.value.map(v => v.length), 2)
+  if (num > 5) {
+    num = 5
+  }
+  return num * 16 + 10
+})
+
+const chartHeight = computed(() => {
+  return (provinceList.value.length || 4) * 60
+})
+
+watch(
+  () => projectInfo,
+  (val) => {
+    provinceList.value = val.provinceList || []
+    updateChart()
+  },
+  { deep: true }
+)
+
+// 初始化
+onMounted(() => {
+  initChart()
+})
+
+// 销毁
+onBeforeUnmount(() => {
+  beforeDestroy()
+})
+
+function beforeDestroy() {
+  if (chart.value) {
+    chart.value.dispose()
+    chart.value = null
+  }
+}
+
+// 更新
+function updateChart() {
+  try {
+    if (chart.value) {
+      chart.value.setOption({
+        grid: setGrid(),
+        yAxis: setYAxis(),
+        series: setSeries()
+      })
+    } else {
+      initChart()
+    }
+    // 改变高度
+    chart.value.resize({ height: chartHeight.value })
+  } catch (error) {
+    console.log('更新chart', error)
+  }
+}
+
+function setYAxis() {
+  return [
+    {
+      type: 'category',
+      data: [...provinceNameList.value],
+      axisLine: { show: false }, // 坐标轴
+      axisTick: [{ // 坐标轴小标记
+        show: false
+      }],
+      axisLabel: {
+        textStyle: {
+          fontSize: '14'
+        }
+      }
+    }
+  ]
+}
+
+function setSeries() {
+  return [
+    {
+      name: '',
+      type: 'bar',
+      tooltip: { show: false },
+      barMinHeight: 30, // 最小柱高
+      barWidth: 40, // 柱宽度
+      barMaxWidth: 100, // 最大柱宽度
+      data: [...quantityList.value],
+      itemStyle: {
+        normal: { // 柱状图颜色
+          color: '#1890ff',
+          label: {
+            show: true, // 显示文本
+            position: 'inside', // 数据值位置
+            textStyle: {
+              fontSize: '14'
+            }
+          }
+        }
+      }
+    }
+  ]
+}
+
+function setGrid() {
+  return [
+    { // 绘图区调整
+      x: fontWidth.value, // 左留白
+      y: 0, // 上留白
+      x2: 10, // 右留白
+      y2: 0 // 下留白
+    }
+  ]
+}
+
+// 初始化
+function initChart() {
+  chart.value = echarts.init(document.getElementById(props.id))
+  chart.value.clear()
+  chart.value.setOption({
+    tooltip: {
+      trigger: 'item' // 悬浮提示框不显示
+    },
+    grid: setGrid(),
+    xAxis: [
+      {
+        show: false,
+        type: 'value',
+        boundaryGap: [0, 0],
+        position: 'top'
+      }
+    ],
+    yAxis: setYAxis(),
+    series: setSeries()
+  })
+}
+</script>
