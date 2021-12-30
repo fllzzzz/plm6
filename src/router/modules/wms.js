fc9587219310834804f07efe58f5fc69338c1d86
DH <2487138213@qq.com>
2021-12-30 15:35:40 +0800

    1.注释路由 原材料-退库审核

--- src/router/modules/wms.js
+++ src/router/modules/wms.js
@@ -117,14 +117,14 @@ export default {
           hidden: false,
           component: '/wms/transfer-application-review/raw-mat/index',
           meta: { title: '原材料-调拨审核', icon: 'project', noCache: true }
-        },
-        {
-          name: 'RawMatReturnApplicationReview',
-          path: 'return-application-review/raw-mat',
-          hidden: false,
-          component: '/wms/return-application-review/raw-mat/index',
-          meta: { title: '原材料-退库审核', icon: 'project', noCache: true }
         }
+        // {
+        //   name: 'RawMatReturnApplicationReview',
+        //   path: 'return-application-review/raw-mat',
+        //   hidden: false,
+        //   component: '/wms/return-application-review/raw-mat/index',
+        //   meta: { title: '原材料-退库审核', icon: 'project', noCache: true }
+        // }
       ]
     },
     {
