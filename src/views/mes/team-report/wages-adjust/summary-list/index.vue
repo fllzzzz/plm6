d1defb62ef30487b15b696596ea3fb64f21e4de4
sangyujie <17826826954@163.com>
2022-03-08 16:38:36 +0800

    F mes-工价调整权限

--- src/views/mes/team-report/wages-adjust/summary-list/index.vue
+++ src/views/mes/team-report/wages-adjust/summary-list/index.vue
@@ -16,7 +16,16 @@
     >
       <el-table-column label="序号" type="index" align="center" width="60" />
       <productType-summary-columns :productType="crud.query.productType" :columns="columns" :unitNewLine="false" />
-      <el-table-column align="center" prop="prop" label="操作" width="100">
+      <el-table-column
+        v-if="
+          (checkPermission(permission.get) && !(crud.query.productType & componentTypeEnum.ASSEMBLE.V)) ||
+          (checkPermission(permission.edit) && crud.query.productType & componentTypeEnum.ASSEMBLE.V)
+        "
+        align="center"
+        prop="prop"
+        label="操作"
+        width="100"
+      >
         <template #default="{ row }">
           <!-- <common-button type="primary" size="mini">全部修改</common-button> -->
           <common-button type="warning" size="mini" @click="handleSeveralEdit(row)">修改</common-button>
@@ -34,6 +43,7 @@ import detailApi from '@/api/mes/team-report/wages-adjust/detail'
 import { ref, provide, defineExpose, defineEmits, inject } from 'vue'
 
 import { componentTypeEnum } from '@enum-ms/mes'
+import checkPermission from '@/utils/system/check-permission'
 
 import useMaxHeight from '@compos/use-max-height'
 import useCRUD from '@compos/use-crud'
