cb336594ff1f7d032798f2f3fffe9e69ca919847
shisanss <15558105385@163.com>
2022-03-22 21:39:40 +0800

    新增 [view] 采购对账管理/付款台账 【30%】

---
+++ src/api/supply-chain/purchase-reconciliation-manage/payment-ledger.js
@@ -0,0 +1,67 @@
+import request from '@/utils/request'
+
+/**
+ * 客户交易列表
+ * @param {number} page|required 页码
+ * @param {number} size|required 页大小
+ * @param {number} endDate 结束时间
+ * @param {number} startDate 开始时间
+ * @param {number} propertyType 属性 1原材料采购 2制成品采购 4原材料运输 8制成品运输 16专业分包
+ * @param {string} supplierName 供应商
+ * @param {string} serialNumber 订单号
+ * @returns
+ */
+export function get(params) {
+  return request({
+    module: 'contract',
+    url: 'supply/chain/order/payment',
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 付款记录
+ * @param {number} orderId|required 订单id
+ * @param {number} propertyType 属性 1原材料采购 2制成品采购 4原材料运输 8制成品运输 16专业分包
+ * @param {number} page|required 页码
+ * @param {number} size|required 页大小
+ */
+export function paymentRecord(params) {
+  return request({
+    module: 'contract',
+    url: `supply/chain/order/payment/${params.orderId}`,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 开票记录
+ * @param {number} orderId|required 订单id
+ * @param {number} propertyType 属性 1原材料采购 2制成品采购 4原材料运输 8制成品运输 16专业分包
+ */
+export function invoiceRecord(params) {
+  return request({
+    module: 'contract',
+    url: `supply/chain/order/payment/invoice/${params.orderId}`,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 入库记录
+ * @param {number} orderId|required 订单id
+ * @param {number} propertyType 属性 1原材料采购 2制成品采购 4原材料运输 8制成品运输 16专业分包
+ */
+export function inboundRecord(params) {
+  return request({
+    module: 'contract',
+    url: `supply/chain/order/payment/inbound/${params.orderId}`,
+    method: 'get',
+    params
+  })
+}
+
+export default { get }
