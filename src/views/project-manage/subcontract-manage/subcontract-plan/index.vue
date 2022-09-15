5c012747103913901649dc951bb53cf1fbcf9cf4
‘xionglinghua2015’ <xionglinghua2015@163.com>
2022-09-15 10:53:57 +0800

    施工资料修改

--- src/views/project-manage/subcontract-manage/subcontract-plan/index.vue
+++ src/views/project-manage/subcontract-manage/subcontract-plan/index.vue
@@ -145,6 +145,7 @@
             </template>
           </el-table-column>
           <el-table-column
+            v-if="checkPermission([...permission.edit])"
             label="操作"
             width="160px"
             align="center"
