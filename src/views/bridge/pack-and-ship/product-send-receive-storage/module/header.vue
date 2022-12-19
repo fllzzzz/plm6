48b76f87e0d271403ed4a5642f81a4c9f89604c6
DESKTOP-9KCFES2\adminpvc <l1511707542@163.com>
2022-12-19 17:51:20 +0800

    桥梁MES： 发运管理 制品入发存

--- src/views/bridge/pack-and-ship/product-send-receive-storage/module/header.vue
+++ src/views/bridge/pack-and-ship/product-send-receive-storage/module/header.vue
@@ -10,39 +10,65 @@
         format="YYYY"
         value-format="x"
         @change="crud.toQuery"
-        style="width:120px;"
+        style="width: 120px"
       />
       <project-radio-button size="small" :type="'all'" v-model="query.projectId" class="filter-item" @change="crud.toQuery" />
       <common-radio-button
-      v-model="query.productType"
-      :options="packTypeEnum.ENUM"
-      type="enum"
-      size="small"
-      class="filter-item"
-      @change="crud.toQuery"
-    />
+        v-model="query.productType"
+        :options="packTypeEnum.ENUM"
+        type="enum"
+        size="small"
+        class="filter-item"
+        @change="crud.toQuery"
+      />
+      <workshop-select
+        v-model="query.workShopId"
+        placeholder="请选择车间"
+        clearable
+        style="width: 200px"
+        class="filter-item"
+        @change="crud.toQuery"
+      />
       <el-row v-loading="summaryLoading" v-if="checkPermission(crud.permission.get)" :gutter="20" class="panel-group">
         <el-col :span="8" class="card-panel-col">
-          <Panel name="累计入库(t)" text-color="#626262" num-color="#1890ff" :endVal="(totalAmount.inboundMete)/1000 || 0"  :precision="DP.COM_WT__KG" />
+          <Panel
+            name="累计入库(t)"
+            text-color="#626262"
+            num-color="#1890ff"
+            :endVal="totalAmount.inboundMete / 1000 || 0"
+            :precision="DP.COM_WT__KG"
+          />
         </el-col>
         <el-col :span="8" class="card-panel-col">
-          <Panel name="累计出库(t)" text-color="#626262" num-color="#1890ff" :endVal="(totalAmount.outboundMete)/1000 || 0" :precision="DP.COM_WT__KG" />
+          <Panel
+            name="累计出库(t)"
+            text-color="#626262"
+            num-color="#1890ff"
+            :endVal="totalAmount.outboundMete / 1000 || 0"
+            :precision="DP.COM_WT__KG"
+          />
         </el-col>
         <el-col :span="8" class="card-panel-col">
-          <Panel name="实时库存(t)" text-color="#626262" num-color="#1890ff" :end-val="(totalAmount.stockMete)/1000 || 0" :precision="DP.COM_WT__KG" />
+          <Panel
+            name="实时库存(t)"
+            text-color="#626262"
+            num-color="#1890ff"
+            :end-val="totalAmount.stockMete / 1000 || 0"
+            :precision="DP.COM_WT__KG"
+          />
         </el-col>
       </el-row>
     </div>
     <crudOperation>
-       <template #viewLeft>
-          <print-table
-            v-permission="crud.permission.print"
-            api-key="bridgeProductSendReceiveStorage"
-            :params="{ ...query }"
-            size="mini"
-            type="warning"
-            class="filter-item"
-          />
+      <template #viewLeft>
+        <print-table
+          v-permission="crud.permission.print"
+          api-key="bridgeProductSendReceiveStorage"
+          :params="{ ...query }"
+          size="mini"
+          type="warning"
+          class="filter-item"
+        />
       </template>
     </crudOperation>
   </div>
@@ -51,6 +77,7 @@
 <script setup>
 import { summaryData } from '@/api/bridge/bridge-pack-and-ship/product-receive-send-storage'
 import { ref, watch } from 'vue'
+import workshopSelect from '@comp-mes/workshop-select'
 import { bridgePackTypeEnum as packTypeEnum } from '@enum-ms/bridge'
 import checkPermission from '@/utils/system/check-permission'
 import { DP } from '@/settings/config'
@@ -62,7 +89,8 @@ import Panel from '@/components/Panel'
 const defaultQuery = {
   dateTime: undefined,
   projectId: undefined,
-  productType: packTypeEnum.BOX.V
+  productType: packTypeEnum.BOX.V,
+  workShopId: undefined
 }
 
 const { crud, query } = regHeader(defaultQuery)
@@ -92,21 +120,20 @@ async function fetchSummaryInfo() {
     summaryLoading.value = false
   }
 }
-
 </script>
 <style lang="scss" scoped>
 .panel-group {
-  margin-bottom:10px;
+  margin-bottom: 10px;
   ::v-deep(.card-panel) {
     .card-panel-description {
       .card-panel-text {
-        text-align:left;
+        text-align: left;
         margin-top: 2px;
       }
       .card-panel-num {
-        display:block;
+        display: block;
         font-size: 17px;
-        text-align:right;
+        text-align: right;
       }
     }
   }
