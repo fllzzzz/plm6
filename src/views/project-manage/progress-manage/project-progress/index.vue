1c11fc0da0cc29a14510a331d6788c17307f3ff8
‘xionglinghua2015’ <xionglinghua2015@163.com>
2023-01-03 17:32:55 +0800

    项目管理-桥梁类型单位修改

--- src/views/project-manage/progress-manage/project-progress/index.vue
+++ src/views/project-manage/progress-manage/project-progress/index.vue
@@ -150,7 +150,7 @@ import { ref, watch } from 'vue'
 import useMaxHeight from '@compos/use-max-height'
 import useCRUD from '@compos/use-crud'
 import { mapGetters } from '@/store/lib'
-import { TechnologyTypeAllEnum, businessTypeEnum } from '@enum-ms/contract'
+import { TechnologyTypeAllEnum, businessTypeEnum, projectTypeEnum } from '@enum-ms/contract'
 import { manufactureTypeEnum, areaPlanTypeEnum } from '@enum-ms/plan'
 import { projectProgressPM as permission } from '@/page-permission/project'
 import { dateDifference } from '@/utils/date'
@@ -232,8 +232,8 @@ CRUD.HOOK.handleRefresh = (crud, data) => {
             value.completedMete = value.completedMete || 0
             value.meteRate = value.completedMete && value.mete ? Number((value.completedMete / value.mete * 100).toFixed(1)) : 0
             value.meteColor = '#1890ff'
-            value.decimal = k.type === TechnologyTypeAllEnum.STRUCTURE.V ? DP.COM_WT__KG : DP.MES_ENCLOSURE_L__M
-            value.unit = k.type === TechnologyTypeAllEnum.STRUCTURE.V ? 't' : 'm'
+            value.decimal = (k.type === TechnologyTypeAllEnum.STRUCTURE.V || globalProject.value.projectType === projectTypeEnum.BRIDGE.V) ? DP.COM_WT__KG : DP.MES_ENCLOSURE_L__M
+            value.unit = (k.type === TechnologyTypeAllEnum.STRUCTURE.V || globalProject.value.projectType === projectTypeEnum.BRIDGE.V) ? 't' : 'm'
           })
         }
         const deepVal = k.detailTraceList.find(m => m.type === areaPlanTypeEnum.DEEPEN.V) || undefined
@@ -259,8 +259,8 @@ CRUD.HOOK.handleRefresh = (crud, data) => {
                 values.completedMete = values.completedMete || 0
                 values.meteRate = values.completedMete && values.mete ? Number((values.completedMete / values.mete * 100).toFixed(1)) : 0
                 values.meteColor = '#1890ff'
-                values.decimal = k.type === TechnologyTypeAllEnum.STRUCTURE.V ? DP.COM_WT__KG : DP.MES_ENCLOSURE_L__M
-                values.unit = k.type === TechnologyTypeAllEnum.STRUCTURE.V ? 't' : 'm'
+                values.decimal = (k.type === TechnologyTypeAllEnum.STRUCTURE.V || globalProject.value.projectType === projectTypeEnum.BRIDGE.V) ? DP.COM_WT__KG : DP.MES_ENCLOSURE_L__M
+                values.unit = (k.type === TechnologyTypeAllEnum.STRUCTURE.V || globalProject.value.projectType === projectTypeEnum.BRIDGE.V) ? 't' : 'm'
               })
             }
             const deepVal = value.planDetailList.find(m => m.type === areaPlanTypeEnum.DEEPEN.V)
