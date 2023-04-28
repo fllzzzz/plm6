9336970ef92e022babab37a2610ebfb3fef22236
DESKTOP-9KCFES2\adminpvc <l1511707542@163.com>
2023-04-28 11:30:41 +0800

    建钢MES: 发运管理/打包记录  结构+配套件 标签预览修改

--- src/views/mes/pack-and-ship/pack-list/module/label-dlg.vue
+++ src/views/mes/pack-and-ship/pack-list/module/label-dlg.vue
@@ -1,7 +1,7 @@
 <template>
   <common-dialog :title="`${packageInfo.serialNumber}`" top="5vh" v-model="dialogVisible" width="600px" :before-close="handleClose">
     <div style="font-weight: bold; font-size: 16pt; color: #333; padding-bottom: 10pt">{{ packageInfo.project.name }}</div>
-    <table border="1" bordercolor="#000000">
+    <table v-if="packageInfo.productType !== 10" border="1" bordercolor="#000000">
       <tr>
         <td rowspan="2">
           <div class="qr-content">
@@ -35,12 +35,12 @@
         <td>数量</td>
       </tr> -->
       <template v-if="packageInfo.productType !== packTypeEnum.AUXILIARY_MATERIAL.V">
-      <tr v-for="(item, index) in breakUpList[page - 1]" :key="index">
-        <td class="col-1">{{ item.serialNumber }}</td>
-        <td class="col-1">{{ item.material }}</td>
-        <td class="col-1">{{ item.quantity }}</td>
-        <td class="col-1">{{ item.totalWeight }}</td>
-      </tr>
+        <tr v-for="(item, index) in breakUpList[page - 1]" :key="index">
+          <td class="col-1">{{ item.serialNumber }}</td>
+          <td class="col-1">{{ item.material }}</td>
+          <td class="col-1">{{ item.quantity }}</td>
+          <td class="col-1">{{ item.totalWeight }}</td>
+        </tr>
       </template>
       <template v-if="packageInfo.productType === packTypeEnum.AUXILIARY_MATERIAL.V">
         <tr v-for="(item, index) in breakUpList[page - 1]" :key="index">
@@ -51,7 +51,7 @@
         </tr>
       </template>
     </table>
-    <table v-if="packageInfo.productType === packTypeEnum.STRUCTURE.V & packTypeEnum.AUXILIARY_MATERIAL.V" border="1" bordercolor="#000000">
+    <table v-if="packageInfo.productType === 10" border="1" bordercolor="#000000">
       <tr>
         <td rowspan="2">
           <div class="qr-content">
@@ -66,13 +66,27 @@
         <td>包单号</td>
         <td colspan="2" style="font-weight: bold">{{ packageInfo.serialNumber }}</td>
       </tr>
-      <tr v-if="packageInfo.productType !== packTypeEnum.AUXILIARY_MATERIAL.V">
+      <tr>
+        <td colspan="4" style="font-weight: bold">结构</td>
+      </tr>
+      <tr>
         <td>编号</td>
         <td>材质</td>
         <td>数量</td>
         <td>重量(kg)</td>
       </tr>
-      <tr v-if="packageInfo.productType === packTypeEnum.AUXILIARY_MATERIAL.V">
+      <!-- <template> -->
+      <tr v-for="(item, index) in structureData" :key="index">
+        <td class="col-1">{{ item.serialNumber }}</td>
+        <td class="col-1">{{ item.material }}</td>
+        <td class="col-1">{{ item.quantity }}</td>
+        <td class="col-1">{{ item.totalWeight }}</td>
+      </tr>
+      <!-- </template> -->
+      <tr>
+        <td colspan="4" style="font-weight: bold">辅材</td>
+      </tr>
+      <tr>
         <td>名称</td>
         <td>单位</td>
         <td>规格</td>
@@ -84,22 +98,15 @@
         <td>长度</td>
         <td>数量</td>
       </tr> -->
-      <template v-if="packageInfo.productType !== packTypeEnum.AUXILIARY_MATERIAL.V">
-      <tr v-for="(item, index) in breakUpList[page - 1]" :key="index">
-        <td class="col-1">{{ item.serialNumber }}</td>
-        <td class="col-1">{{ item.material }}</td>
-        <td class="col-1">{{ item.quantity }}</td>
-        <td class="col-1">{{ item.totalWeight }}</td>
+
+      <!-- <template> -->
+      <tr v-for="(aux, index) in auxData" :key="index">
+        <td class="col-1">{{ aux.name }}</td>
+        <td class="col-1">{{ aux.measureUnit }}</td>
+        <td class="col-1">{{ aux.specification }}</td>
+        <td class="col-1">{{ aux.quantity }}</td>
       </tr>
-      </template>
-      <template v-if="packageInfo.productType === packTypeEnum.AUXILIARY_MATERIAL.V">
-        <tr v-for="(item, index) in breakUpList[page - 1]" :key="index">
-          <td class="col-1">{{ item.name }}</td>
-          <td class="col-1">{{ item.measureUnit }}</td>
-          <td class="col-1">{{ item.specification }}</td>
-          <td class="col-1">{{ item.quantity }}</td>
-        </tr>
-      </template>
+      <!-- </template> -->
     </table>
     <el-pagination
       style="text-align: center; margin-top: 10px"
@@ -143,6 +150,20 @@ const breakUpList = computed(() => {
     _list.push(packageInfo.value.list.slice(i, Math.min(i + 11, len)))
   }
   return _list
+})
+const structureData = computed(() => {
+  const _structureData = []
+  for (var i = 0, len = packageInfo.value.structureList.length; i < len; i++) {
+    _structureData.push(packageInfo.value.structureList[i])
+  }
+  return _structureData
+})
+const auxData = computed(() => {
+  const _auxData = []
+  for (var i = 0, len = packageInfo.value.auxList.length; i < len; i++) {
+    _auxData.push(packageInfo.value.auxList[i])
+  }
+  return _auxData
 })
 </script>
 
