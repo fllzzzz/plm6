55eeda90a9a6501842557d6a3e00e99be06fffb7
DESKTOP-9KCFES2\adminpvc <l1511707542@163.com>
2023-05-10 18:43:05 +0800

    通过接口返回字段判断是否存在智能线

--- src/utils/enum/modules/mes.js
+++ src/utils/enum/modules/mes.js
@@ -330,6 +330,12 @@ const artifactProductLineEnum = {
 }
 constantize(artifactProductLineEnum)
 
+// 构件配置传统/智能生产线
+const traditionLineEnum = {
+  TRADITION: { L: '传统线', K: 'TRADITION', V: 1 << 0 }
+}
+constantize(traditionLineEnum)
+
 // 构件配置智能线父类型
 const intellectParentType = {
   PILLAR: { L: '柱', K: 'PILLAR', V: 1 << 1 },
@@ -640,6 +646,7 @@ export {
   inProductionDetailReportEnum,
   schedulingStatusEnum,
   artifactProductLineEnum,
+  traditionLineEnum,
   intellectParentType,
   minEqualTypeEnum,
   maxEqualTypeEnum,
@@ -717,6 +724,7 @@ export default {
   inProductionDetailReportEnum,
   schedulingStatusEnum,
   artifactProductLineEnum,
+  traditionLineEnum,
   intellectParentType,
   minEqualTypeEnum,
   maxEqualTypeEnum,
