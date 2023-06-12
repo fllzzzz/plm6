34bd7fe93fd88bae2c2cd99f9aee149dac8c4285
shisanss <15558105385@163.com>
2023-06-12 16:55:32 +0800

    feat：新增外链大屏接口。

--- src/api/external/model.js
+++ src/api/external/model.js
@@ -1,12 +1,25 @@
 import request from '@/utils/request'
 
 /**
- * 获取大屏模型
+ * 根据文件id获取大屏模型
  * @param {number} fileId | required 文件id
  */
 export function getModel(url, params) {
   return request({
     url: `${url}/api/largeScreen/getModelInfoByFileId`,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 根据单体id获取大屏模型
+ * @param {number} monomerId | required 单体id
+ * @param {number} areaId | required 区域id
+ */
+export function getTranslate(url, params) {
+  return request({
+    url: `${url}/api/largeScreen/translate`,
     method: 'get',
     params
   })
