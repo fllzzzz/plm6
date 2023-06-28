3220f6f7ada51d32f757355e3de97b6860f348a6
DESKTOP-9KCFES2\adminpvc <l1511707542@163.com>
2023-06-28 09:30:56 +0800

    计划管理： 技术成果/XML、数控文件管理页面增加匹配导入按钮

--- src/views/plan/technical-data-manage/technical-achievement/cnc/index.vue
+++ src/views/plan/technical-data-manage/technical-achievement/cnc/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <!--工具栏-->
-    <mHeader class="head-container" :globalProject="globalProject"/>
+    <mHeader class="head-container" :globalProject="globalProject" />
     <!--表格渲染-->
     <common-table
       ref="tableRef"
@@ -11,43 +11,93 @@
       :max-height="maxHeight"
       style="width: 100%"
     >
-    <el-table-column prop="index" label="序号" align="center" width="60" type="index" />
-    <el-table-column v-if="columns.visible('monomerName')" key="monomerName" prop="monomerName" show-overflow-tooltip label="单体" align="center"/>
-    <el-table-column v-if="columns.visible('quantity')" key="quantity" prop="quantity" show-overflow-tooltip :label="`${globalProject.projectType === projectTypeEnum.STEEL.V?deepenTypeEnum.V?.[crud.query.productType]?.L:bridgeProcessTypeEnum.V?.[crud.query.productType]?.L}总数`" align="center"/>
-    <el-table-column v-if="columns.visible('drawingQuantity')" key="drawingQuantity" prop="drawingQuantity" show-overflow-tooltip label="文件匹配总数" align="center"/>
-    <!--编辑与删除-->
-    <el-table-column
-      v-if="checkPermission([...permission.import, ...permission.detail])"
-      label="操作"
-      width="170px"
-      align="center"
-      fixed="right"
-    >
-      <template v-slot="scope">
-        <div class="btn">
-          <upload-btn
-            v-permission="permission.import"
-            :dataType="scope.row.dataType"
-            :accept="`.zip,${uploadType}`"
-            :data="getParams(scope.row)"
-            :tip="uploadType"
-            size="mini"
-            type="primary"
-            @success="crud.toQuery"
-          />
-          <common-button v-permission="permission.detail" type="primary" size="mini" icon="el-icon-document" @click="openDetail(scope.row)" />
-        </div>
-      </template>
-    </el-table-column>
-  </common-table>
-  <!--分页组件-->
-  <pagination />
-  <detail v-model="detailVisible" :currentRow="currentRow" :permission="permission" :upload-type="uploadType" :tip="`覆盖导入仅支持${tip}格式`" @success="crud.toQuery"/>
+      <el-table-column prop="index" label="序号" align="center" width="60" type="index" />
+      <el-table-column
+        v-if="columns.visible('monomerName')"
+        key="monomerName"
+        prop="monomerName"
+        show-overflow-tooltip
+        label="单体"
+        align="center"
+      />
+      <el-table-column
+        v-if="columns.visible('quantity')"
+        key="quantity"
+        prop="quantity"
+        show-overflow-tooltip
+        :label="`${
+          globalProject.projectType === projectTypeEnum.STEEL.V
+            ? deepenTypeEnum.V?.[crud.query.productType]?.L
+            : bridgeProcessTypeEnum.V?.[crud.query.productType]?.L
+        }总数`"
+        align="center"
+      />
+      <el-table-column
+        v-if="columns.visible('drawingQuantity')"
+        key="drawingQuantity"
+        prop="drawingQuantity"
+        show-overflow-tooltip
+        label="文件匹配总数"
+        align="center"
+      />
+      <!--编辑与删除-->
+      <el-table-column
+        v-if="checkPermission([...permission.import, ...permission.detail])"
+        label="操作"
+        width="260px"
+        align="center"
+        fixed="right"
+      >
+        <template v-slot="scope">
+          <div class="btn">
+            <upload-btn
+              v-permission="permission.import"
+              :dataType="scope.row.dataType"
+              :accept="`.zip,${uploadType}`"
+              :data="getParams(scope.row)"
+              :tip="uploadType"
+              size="mini"
+              type="primary"
+              @success="crud.toQuery"
+            />
+            <upload-btn
+              :dataType="scope.row.dataType"
+              :uploadFun="uploadChoose"
+              :accept="`.zip`"
+              :data="getParams(scope.row)"
+              :tip="uploadType"
+              btnName="匹配导入"
+              size="mini"
+              btnType="warning"
+              style="margin-right: 6px"
+              @success="crud.toQuery"
+            />
+            <common-button
+              v-permission="permission.detail"
+              type="primary"
+              size="mini"
+              icon="el-icon-document"
+              @click="openDetail(scope.row)"
+            />
+          </div>
+        </template>
+      </el-table-column>
+    </common-table>
+    <!--分页组件-->
+    <pagination />
+    <detail
+      v-model="detailVisible"
+      :currentRow="currentRow"
+      :permission="permission"
+      :upload-type="uploadType"
+      :tip="`覆盖导入仅支持${tip}格式`"
+      @success="crud.toQuery"
+    />
   </div>
 </template>
 
 <script setup>
-import crudApi from '@/api/plan/technical-data-manage/technical-achievement'
+import crudApi, { uploadChoose } from '@/api/plan/technical-data-manage/technical-achievement'
 import { ref, watch, computed } from 'vue'
 import { mapGetters } from '@/store/lib'
 
@@ -77,8 +127,8 @@ const tableRef = ref()
 const currentRow = ref({})
 const detailVisible = ref(false)
 
-const uploadType = computed(() => crud.query.productType === deepenTypeEnum.ASSEMBLY.V ? '.nc1' : '.dxf')
-const tip = computed(() => crud.query.productType === deepenTypeEnum.ASSEMBLY.V ? 'NC1' : 'DXF')
+const uploadType = computed(() => (crud.query.productType === deepenTypeEnum.ASSEMBLY.V ? '.nc1' : '.dxf'))
+const tip = computed(() => (crud.query.productType === deepenTypeEnum.ASSEMBLY.V ? 'NC1' : 'DXF'))
 
 const { crud, columns } = useCRUD(
   {
@@ -125,7 +175,7 @@ function openDetail(row) {
 <style lang="scss" scoped>
 ::v-deep(.el-table) {
   .btn {
-    *:nth-child(2){
+    *:nth-child(2) {
       margin-left: 6px;
     }
   }
