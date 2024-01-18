5d25a20719bb6de3a05ae4073576163fb9214b5e
郑惠继 <z13656878163@163.com>
2024-01-18 18:24:20 +0800

    常规标签样式问题

--- src/utils/label/constant.js
+++ src/utils/label/constant.js
@@ -54,6 +54,7 @@ const ARTIFACT_COMMON_L_HTML = function ({ component = defComponent, printConfig
         <td rowspan="3" class="qr-content"></td>
       </tr>
       <tr style="${!printConfig?.showArea && !component?.oneCode ? 'border-bottom:1px solid #000;' : ''}">
+      <td colspan="2" style="${!printConfig?.showArea && !component?.oneCode ? '' : 'display:none;'}"></td>
         <td colspan="${printConfig?.showArea && component?.oneCode ? 1 : 2}" style="${printConfig?.showArea ? '' : 'display:none;'}">区域：${emptyTextFormatter(component.areaName)}</td>
         <td colspan="${printConfig?.showArea && component?.oneCode ? 1 : 2}" style="${component?.oneCode ? '' : 'display:none;'}">编码：${component.oneCode}</td>
       </tr>
